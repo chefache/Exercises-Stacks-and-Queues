58728d0e3036582db25022ff9650f6f4d372f99a
Stefan Koev <stefan.m.koev@gmail.com>
2019-12-11 20:01:57 +0200

    Add files via upload

---
+++ Matching Brackets/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Matching_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //  1 + (2 - (2 + 3) * 4 / (3 + 1)) * 5
+
+
+            string input = Console.ReadLine();
+
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var @char = input[i];
+               
+                if (@char == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (@char == ')')
+                {
+                    var firstOpen = stack.Pop();
+                    var exprssion = input.Substring(firstOpen, i - firstOpen + 1);
+                    Console.WriteLine(exprssion);
+                }
+            }
+   
+        }
+    }
+}
---
+++ Simple Calculator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2 + 5 + 10 - 2 - 1	14
+
+            var inputString = Console.ReadLine();
+            var inputExpression =  inputString.Split(" ");
+
+            var myStack = new Stack<string>(inputExpression.Reverse());
+
+            var result = 0;
+
+            while (myStack.Count > 0)
+            {
+                var currentSymbol = myStack.Pop();
+                if (currentSymbol == "+")
+                {
+                    var number = myStack.Pop();
+                    result += int.Parse(number);
+
+                }
+                else if (currentSymbol == "-")
+                {
+                    var number = myStack.Pop();
+                    result -= int.Parse(number);
+                }
+                else
+                {
+                    result = int.Parse(currentSymbol);
+                }
+            }
+            Console.WriteLine(result);
+
+        }
+    }
+}
